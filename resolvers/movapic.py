06e559baeb3b54e53365ba5ec5af219acc1883ba
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-08-25 19:44:38 +0900

    携帯百景を実装

---
+++ resolvers/movapic.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+import re
+import MySQLdb
+import urllib2
+from private_constant import *
+
+class movapic:
+	regexStr = "^http://(?:www\\.)?movapic\\.com/(?:(\\w+)/pic/(\\d+)|pic/(\w+))/?(?:\\?.*)?$"
+	regex = re.compile(regexStr, re.IGNORECASE)
+	
+	imageRegex = re.compile("<img class=\"image\" src=\"http://image\\.movapic\\.com/pic/m_(\\w+)\\.jpeg\"/>")
+	
+	last = None
+	
+	def getUriData(self, match):
+		expanded = match.group(3)
+		
+		if expanded is None:
+			username = match.group(1)
+			id = match.group(2)
+			
+			if self.last is not None and str(self.last[0]) == str(id):
+				return self.last
+			
+			db = MySQLdb.connect(user=dbName, passwd=dbPassword, db=dbName, charset="utf8")
+			c = db.cursor()
+			
+			c.execute("SELECT * FROM movapic WHERE id = " + db.literal(id))
+			sqlResult = c.fetchone()
+			
+			if sqlResult is None:
+				httpRes = None
+				
+				httpRes = urllib2.urlopen("http://movapic.com/%s/pic/%s" % (username, id))
+				
+				html = httpRes.read().decode("utf-8")
+				
+				htmlMatch = self.imageRegex.search(html)
+				
+				if htmlMatch is None:
+					return None
+				
+				expanded = htmlMatch.group(1)
+				
+				c.execute("INSERT INTO movapic VALUES (%s, %s)"
+					% (db.literal(id), db.literal(expanded))
+				)
+				
+				db.commit()
+				
+				self.last = [id, expanded]
+			else:
+				self.last = sqlResult
+			
+			return self.last
+		else:
+			return ["", expanded]
+			
+	def getFullSize(self, match):
+		data = self.getUriData(match)
+		
+		if data is None:
+			return None
+		else:
+			return "http://image.movapic.com/pic/_%s.jpeg" % data[1]
+	
+	def getLargeSize(self, match):
+		data = self.getUriData(match)
+		
+		if data is None:
+			return None
+		else:
+			return "http://image.movapic.com/pic/m_%s.jpeg" % data[1]
+	
+	def getThumbnail(self, match):
+		data = self.getUriData(match)
+		
+		if data is None:
+			return None
+		else:
+			return "http://image.movapic.com/pic/t_%s.jpeg" % data[1]
--- supported.py
+++ supported.py
@@ -8,6 +8,7 @@ import gyazo
 import hatena_fotolife
 import imgly
 import lockerz
+import movapic
 import niconico_seiga
 import pckles
 import photozou
@@ -23,6 +24,7 @@ services = [
 	hatena_fotolife.hatenaFotolife(),
 	imgly.imgly(),
 	lockerz.lockerz(),
+	movapic.movapic(),
 	niconico_seiga.niconicoSeiga(),
 	pckles.pckles(),
 	photozou.photozou(),
