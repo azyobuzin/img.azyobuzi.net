8e8e0d359b3cce309b39b88650bd8d31486f7598
azyobuzin <azyobuzin@users.sourceforge.jp>
2012-10-31 21:23:01 +0900

    private_constant.py の文字コードを修正

--- mirroring/private_constant.py
+++ mirroring/private_constant.py
@@ -10,7 +10,7 @@ tinamiApiKey = "503cb18fee10e"
 tumblrApiKey = "Rf22FFPnVuWLa9HaRQCPvIMfvTAZYcVRJXLdsg59lkYsnpsTLU"
 ustreamApiKey = "E684BEE3166534D6B6A835F62BD535A3"
 
-# @imgazyobuziÅEReadOnly å†å¿
+# @imgazyobuzi„ÉªReadOnly Ê®©Èôê
 twitterOAuthClient = oauth2.Client(
 	oauth2.Consumer(key="uiYQy5R2RJFZRZ4zvSk7A", secret="qzDldacVrcyXbp8pBerf1LBfnQXmkPKmyLVGGLus8"),
 	oauth2.Token(key="862962650-rIcjsj0j9ZJ8khPVA8jZTtEJuq7YYDBDpx6fOAgb", secret="kbMQjdVldI6tFOST3SVjmyAtG1D0oCkCpL6vBv1FtA")
