25baa408dab98b68fcdbb425d3c27bd2b1d9dbf8
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-03-31 00:12:18 +0900

    DB 保存・OpenGraph を読み取る系の Resolver を作ったが未テスト

--- src/resolvers/__init__.py
+++ src/resolvers/__init__.py
@@ -2,6 +2,12 @@
 
 import abc
 import re
+from sgmllib import SGMLParser
+import urllib2
+
+import MySQLdb
+
+from private_constant import *
 
 class Resolver(object):
     __metaclass__ = abc.ABCMeta
@@ -11,7 +17,7 @@ class Resolver(object):
 
     @abc.abstractproperty
     def service_name(self):
-        return "img.azyobuzi.net"
+        return None
 
     @abc.abstractproperty
     def regex_str(self):
@@ -54,3 +60,85 @@ class Resolver(object):
     @abc.abstractmethod
     def get_video_https(self, match):
         return None
+
+class StoringResolver(Resolver):
+    "DB に保存するタイプのリゾルバー"
+
+    def __init__(self):
+        super(StoringResolver, self).__init__()
+        self._cached_param = None
+        self._cached_result = None
+
+    def _connect_db(self):
+        return MySQLdb.connect(user=db_user, passwd=db_password, db=db_name, charset="utf8")
+
+    @abc.abstractmethod
+    def get_parameters(self, match):
+        "match の結果から DB 参照時に使うパラメータを取得する"
+        pass
+
+    @abc.abstractmethod
+    def _work(self, param, db, cursor):
+        pass
+
+    def work(self, match):
+        param = self.get_parameters(match)
+        if param == self._cached_param:
+            return self._cached_result
+        else:
+            with self._connect_db() as db:
+                c = db.cursor()
+                try:
+                    result = self._work(db, c)
+                    self._cached_param = param
+                    self._cached_result = result
+                    return result
+                finally:
+                    c.close()
+
+    @staticmethod
+    def select_one(cursor, table, columns, conditions):
+        query = "SELECT %s FROM %s WHERE %s" \
+            % (", ".join(columns), table, " AND ".join("%s = %%s" % c for c in conditions.iterkeys()))
+        cursor.execute(query, tuple(conditions.itervalues()))
+        return c.fetchone()
+
+    @staticmethod
+    def insert_all(db, cursor, table, values):
+        "カラムを指定しないで INSERT INTO"
+        query = "INSERT INTO %s VALUES(%s)" % (table, ", ".join(["%s"] * len(values)))
+        cursor.execute(query, tuple(values))
+        db.commit()
+
+class OpenGraphResolver(StoringResolver):
+    class OpenGraphParser(SGMLParser):
+        def __init__(self):
+            SGMLParser.__init__(self)
+            self.uri = None
+
+        def do_meta(self, attributes):
+            dic = dict(attributes)
+            if dic.get("property") == "og:image":
+                self.uri = dic["content"]
+
+    @staticmethod
+    def read_og(uri, check=None, encoding="utf-8"):
+        u"""OpenGraph を読み込みに行きます。
+
+        uri: 取得しに行く URI
+        check: 正しいレスポンスかどうかを確認するラムダ式
+
+        成功時には og:image の内容を、 check が False を返した場合には False を、 og:image が見つからなかった場合には None を返します。
+        """
+        #あとでわからなくなりそうなので書いておいた
+
+        response = urllib2.urlopen(uri)
+
+        if check is not None and not check(response):
+            return False
+
+        parser = OpenGraphParser()
+        parser.feed(response.read().decode(encoding))
+        parser.close()
+
+        return parser.uri
--- src/resolvers/twitpic.py
+++ src/resolvers/twitpic.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
 
-##import re
-##
-##class twitpic:
-##	def __str__(self):
-##		return "Twitpic"
-##
-##	regexStr = "^https?://(?:www\\.)?twitpic\\.com/(?:show/\\w+/)?(\\w+)/?(?:\\?.*)?$"
-##	regex = re.compile(regexStr, re.IGNORECASE)
-##
-##	def getFullSize(self, match):
-##		return "http://twitpic.com/show/full/" + match.group(1)
-##
-##	def getLargeSize(self, match):
-##		return "http://twitpic.com/show/large/" + match.group(1)
-##
-##	def getThumbnail(self, match):
-##		return "http://twitpic.com/show/thumb/" + match.group(1)
-
-from resolvers import Resolver
+from resolvers import *
 
 class Twitpic(Resolver): #メモ：スキーマを https にしても結局 http にリダイレクトされる
     @property
