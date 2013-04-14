d42b424d7b4d40634c786ede8ba7e21d157c13e8
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-14 20:58:46 +0900

    img.ly を実装

--- src/resolvers/imgly.py
+++ src/resolvers/imgly.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class imgly:
-	def __str__(self):
-		return "img.ly"
-	
-	regexStr = "^https?://(?:www\\.)?img\\.ly/(?:show/\\w+/)?(\\w+)/?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://img.ly/show/full/" + match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://img.ly/show/large/" + match.group(1)
-	
-	def getThumbnail(self, match):
-		return "http://img.ly/show/thumb/" + match.group(1)
+class Imgly(Resolver): #メモ：twitpic 同様 http にリダイレクトされる
+    @property
+    def service_name(self):
+        return "img.ly"
+
+    @property
+    def regex_str(self):
+        return r"^https?://(?:www\.)?img\.ly/(?:show/\w+/)?(\w+)/?(?:\?.*)?$"
+
+    def get_full(self, match):
+        return "http://img.ly/show/full/" + match.group(1)
+
+    def get_full_https(self, match):
+        return "https://img.ly/show/full/" + match.group(1)
+
+    def get_large(self, match):
+        return "http://img.ly/show/large/" + match.group(1)
+
+    def get_large_https(self, match):
+        return "https://img.ly/show/large/" + match.group(1)
+
+    def get_thumb(self, match):
+        return "http://img.ly/show/thumb/" + match.group(1)
+
+    def get_thumb_https(self, match):
+        return "https://img.ly/show/thumb/" + match.group(1)
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
