80ed03d14c04719250ab25a3b97db19c7c4908f3
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-14 21:42:41 +0900

    Lockerz を実装

--- src/resolvers/lockerz.py
+++ src/resolvers/lockerz.py
@@ -1,20 +1,38 @@
 # -*- coding: utf-8 -*-
 
-import re
-import urllib2
+import urllib
 
-class lockerz:
-	def __str__(self):
-		return "Lockerz"
-	
-	regexStr = "^https?://(?:(?:www\\.|pics\\.)?lockerz\\.com/(?:s|u/\\d+/photos)|(?:www\\.)?plixi\\.com/p)/(\\d+)(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=big&url=" + urllib2.quote("http://lockerz.com/s/" + match.group(1))
-	
-	def getLargeSize(self, match):
-		return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=medium&url=" + urllib2.quote("http://lockerz.com/s/" + match.group(1))
-	
-	def getThumbnail(self, match):
-		return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=small&url=" + urllib2.quote("http://lockerz.com/s/" + match.group(1))
+from resolvers import *
+
+class Lockerz(Resolver):
+    @property
+    def service_name(self):
+        return "Lockerz"
+
+    @property
+    def regex_str(self):
+        return r"^https?://(?:(?:www\.|pics\.)?lockerz\.com/(?:s|u/\d+/photos)|(?:www\.)?plixi\.com/p)/(\d+)(?:\?.*)?$"
+
+    def get_full(self, match):
+        return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=big&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_full_https(self, match):
+        return "https://api.plixi.com/api/tpapi.svc/imagefromurl?size=big&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_large(self, match):
+        return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=medium&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_large_https(self, match):
+        return "https://api.plixi.com/api/tpapi.svc/imagefromurl?size=medium&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_thumb(self, match):
+        return "http://api.plixi.com/api/tpapi.svc/imagefromurl?size=small&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_thumb_https(self, match):
+        return "https://api.plixi.com/api/tpapi.svc/imagefromurl?size=small&url=" + urllib.quote("http://lockerz.com/s/" + match.group(1))
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
