425a3eb1f5b5b326cb545d0bf6c26fd442b38cf2
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-14 21:52:39 +0900

    Mobypicture を実装

--- src/resolvers/mobypicture.py
+++ src/resolvers/mobypicture.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class mobypicture:
-	def __str__(self):
-		return "Mobypicture"
-	
-	regexStr = "^http://(?:www\\.)?moby\\.to/(\\w+)(?:\\:\\w*)?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://moby.to/%s:full" % match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://moby.to/%s:medium" % match.group(1)
-	
-	def getThumbnail(self, match):
-		return "http://moby.to/%s:thumb" % match.group(1)
+class Mobypicture(Resolver): #メモ：twitpic 同様 http にリダイレクトされる
+    @property
+    def service_name(self):
+        return "Mobypicture"
+
+    @property
+    def regex_str(self):
+        return r"^http://(?:www\.)?moby\.to/(\w+)(?:\:\w*)?(?:\?.*)?$"
+
+    def get_full(self, match):
+        return "http://moby.to/%s:full" % match.group(1)
+
+    def get_full_https(self, match):
+        return "https://moby.to/%s:full" % match.group(1)
+
+    def get_large(self, match):
+        return "http://moby.to/%s:medium" % match.group(1)
+
+    def get_large_https(self, match):
+        return "https://moby.to/%s:medium" % match.group(1)
+
+    def get_thumb(self, match):
+        return "http://moby.to/%s:thumb" % match.group(1)
+
+    def get_thumb_https(self, match):
+        return "https://moby.to/%s:thumb" % match.group(1)
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
