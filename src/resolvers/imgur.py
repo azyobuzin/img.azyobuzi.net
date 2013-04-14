7c1c628e71587413237c917c1abe5a9bd9530292
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-14 21:17:39 +0900

    imgur を実装

--- src/resolvers/imgur.py
+++ src/resolvers/imgur.py
@@ -1,21 +1,38 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class imgur:
-	def __str__(self):
-		return "imgur"
-	
-	regexStr = "^https?://(?:www\\.|i\\.)?imgur\\.com/(\\w+)(?:\\.\\w+)?/?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	#拡張子関係なく元のデータが返ってくる
-	
-	def getFullSize(self, match):
-		return "http://i.imgur.com/%s.jpg" % match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://i.imgur.com/%sl.jpg" % match.group(1)
-	
-	def getThumbnail(self, match):
-		return "http://i.imgur.com/%ss.jpg" % match.group(1)
+class Imgur(Resolver):
+    @property
+    def service_name(self):
+        return "imgur"
+
+    @property
+    def regex_str(self):
+        return r"^https?://(?:www\.|i\.)?imgur\.com/(?:gallery/)?(\w+)(?:\.\w+)?/?(?:\?.*)?$"
+
+    #拡張子関係なく元のデータが返ってくる
+
+    def get_full(self, match):
+        return "http://i.imgur.com/%s.jpg" % match.group(1)
+
+    def get_full_https(self, match):
+        return None
+
+    def get_large(self, match):
+        return "http://i.imgur.com/%sl.jpg" % match.group(1)
+
+    def get_large_https(self, match):
+        return None
+
+    def get_thumb(self, match):
+        return "http://i.imgur.com/%ss.jpg" % match.group(1)
+
+    def get_thumb_https(self, match):
+        return None
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
