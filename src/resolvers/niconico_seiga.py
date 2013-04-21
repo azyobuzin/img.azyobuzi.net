accf2adad7736e51ba7b47308f640b80741c6bce
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-21 13:34:15 +0900

    ニコニコ静画を実装

--- src/resolvers/niconico_seiga.py
+++ src/resolvers/niconico_seiga.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class niconicoSeiga:
-	def __str__(self):
-		return u"ニコニコ静画"
-	
-	regexStr = "^https?://(?:seiga\\.nicovideo\\.jp/seiga|nico\\.ms)/im(\\d+)(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://seiga.nicovideo.jp/image/source?id=" + match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://lohas.nicoseiga.jp/thumb/" + match.group(1) + "i"
-	
-	def getThumbnail(self, match):
-		return "http://lohas.nicoseiga.jp/thumb/" + match.group(1)
+class NiconicoSeiga(Resolver):
+    @property
+    def service_name(self):
+        return u"ニコニコ静画"
+
+    @property
+    def regex_str(self):
+        return r"^https?://(?:seiga\.nicovideo\.jp/seiga|nico\.ms)/im(\d+)(?:\?.*)?$"
+
+    def get_full(self, match):
+        return "http://seiga.nicovideo.jp/image/source?id=" + match.group(1)
+
+    def get_full_https(self, match):
+        return None
+
+    def get_large(self, match):
+        return "http://lohas.nicoseiga.jp/thumb/%si" % match.group(1)
+
+    def get_large_https(self, match):
+        return None
+
+    def get_thumb(self, match):
+        return "http://lohas.nicoseiga.jp/thumb/" + match.group(1)
+
+    def get_thumb_https(self, match):
+        return None
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
