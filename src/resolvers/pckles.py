1928afcf956e9c1920ad76d004e7386c5f6567a3
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-22 16:56:25 +0900

    pckles を実装

--- src/resolvers/pckles.py
+++ src/resolvers/pckles.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class pckles:
-	def __str__(self):
-		return "Pckles"
-	
-	regexStr = "^https?://pckles\\.com/(\w+)/(\w+)(?:/|\\.png|\\.(?:resize\\.)?jpg)?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://pckles.com/%s/%s.png" % (match.group(1), match.group(2))
-	
-	def getLargeSize(self, match):
-		return "http://pckles.com/%s/%s.jpg" % (match.group(1), match.group(2))
-	
-	def getThumbnail(self, match):
-		return "http://pckles.com/%s/%s.resize.jpg" % (match.group(1), match.group(2))
+class Pckles(Resolver):
+    @property
+    def service_name(self):
+        return "Pckles"
+
+    @property
+    def regex_str(self):
+        return r"^https?://pckles\.com/(\w+)/(\w+)(?:/|\.png|\.(?:resize\.)?jpg)?(?:\?.*)?$"
+
+    def get_full(self, match):
+        return "http://pckles.com/%s/%s.png" % (match.group(1), match.group(2))
+
+    def get_full_https(self, match):
+        return "https://pckles.com/%s/%s.png" % (match.group(1), match.group(2))
+
+    def get_large(self, match):
+        return "http://pckles.com/%s/%s.jpg" % (match.group(1), match.group(2))
+
+    def get_large_https(self, match):
+        return "https://pckles.com/%s/%s.jpg" % (match.group(1), match.group(2))
+
+    def get_thumb(self, match):
+        return "http://pckles.com/%s/%s.resize.jpg" % (match.group(1), match.group(2))
+
+    def get_thumb_https(self, match):
+        return "https://pckles.com/%s/%s.resize.jpg" % (match.group(1), match.group(2))
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
