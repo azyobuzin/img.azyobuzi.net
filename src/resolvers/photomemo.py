bdf86efd7763d2375f7b7ecaf876c220d68db565
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-24 20:57:57 +0900

    Photomemo を実装

--- src/resolvers/photomemo.py
+++ src/resolvers/photomemo.py
@@ -1,75 +1,59 @@
 # -*- coding: utf-8 -*-
 
-import re
-import MySQLdb
-import urllib2
-from sgmllib import SGMLParser
-from private_constant import *
+from resolvers import *
 
-class PhotomemoParser(SGMLParser):
-    def __init__(self):
-        SGMLParser.__init__(self)
-        self.filename = None
-
-    def do_meta(self, attributes):
-        dic = dict(attributes)
-        if "property" in dic and dic["property"] == "og:image":
-            content = dic["content"]
-            self.filename = content[len("http://photomemo.jp/show_image/panel/") :]
-
-class photomemo:
-    def __str__(self):
+class Photomemo(OpenGraphResolver):
+    @property
+    def service_name(self):
         return "Photomemo"
 
-    regexStr = "^https?://(?:www\\.)?photomemo\\.jp/\\w+/(\\d+)/?(?:\\?.*)?$"
-    regex = re.compile(regexStr, re.IGNORECASE)
+    @property
+    def regex_str(self):
+        return r"^https?://(?:www\.)?photomemo\.jp/\w+/(\d+)/?(?:\?.*)?$"
 
-    last = None
+    def get_parameters(self, match):
+        return match.group(1)
 
-    def getUriData(self, match):
-        id = match.group(1)
+    def _work(self, param, cursor):
+        table = "photomemo"
+        columns = ["id", "filename"]
+        result = self.select_one(cursor, table, columns[1:], {columns[0]: param})
+        if result:
+            return result[0]
 
-        if self.last is not None and str(self.last[0]) == id:
-            return self.last
+        try:
+            uri = self.read_og("http://photomemo.jp/p/" + param)
+        except urllib2.HTTPError as e:
+            if e.code == 404:
+                raise PictureNotFoundError()
+            else:
+                raise e
 
-        db = MySQLdb.connect(user=dbName, passwd=dbPassword, db=dbName, charset="utf8")
-        c = db.cursor()
+        filename = uri[len("http://photomemo.jp/show_image/panel/"):]
 
-        c.execute("SELECT * FROM photomemo WHERE id = %s", id)
-        sqlResult = c.fetchone()
+        self.insert_all(cursor, table, (param, filename))
+        return filename
 
-        if sqlResult is None:
-            httpRes = None
-            try:
-                httpRes = urllib2.urlopen("http://photomemo.jp/p/" + id)
-            except:
-                return None
+    def get_full(self, match):
+        return "http://photomemo.jp/show_image/show_image/" + self.work(match)
 
-            html = httpRes.read().decode("utf-8")
+    def get_full_https(self, match):
+        return None
 
-            parser = PhotomemoParser()
-            parser.feed(html)
-            parser.close()
+    def get_large(self, match):
+        return self.get_full(match)
 
-            filename = parser.filename
+    def get_large_https(self, match):
+        return None
 
-            c.execute("INSERT INTO photomemo VALUES (%s, %s)", (id, filename))
-            db.commit()
+    def get_thumb(self, match):
+        return "http://photomemo.jp/show_image/panel/" + self.work(match)
 
-            self.last = (id, filename)
-        else:
-            self.last = sqlResult
+    def get_thumb_https(self, match):
+        return None
 
-        return self.last
+    def get_video(self, match):
+        return None
 
-    def getFullSize(self, match):
-        data = self.getUriData(match)
-        return "http://photomemo.jp/show_image/show_image/" + data[1] if data is not None else None
-
-    def getLargeSize(self, match):
-        data = self.getUriData(match)
-        return "http://photomemo.jp/show_image/show_image/" + data[1] if data is not None else None
-
-    def getThumbnail(self, match):
-        data = self.getUriData(match)
-        return "http://photomemo.jp/show_image/panel/" + data[1] if data is not None else None
+    def get_video_https(self, match):
+        return None
