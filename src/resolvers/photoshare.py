857763a57de0645e8be536f2bb55172ab9377151
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-24 21:06:54 +0900

    PhotoShare 死んでる

--- src/resolvers/photoshare.py
+++ src/resolvers/photoshare.py
@@ -1,22 +1,41 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class photoshare:
-	def __str__(self):
-		return "Big Canvas PhotoShare"
-	
-	regexStr = "^http://(?:www\\.)?bcphotoshare\\.com/photos/\\d+/(\\d+)/?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	#large.jpgとoriginal.jpgは同じ
-	#拡張子は関係ない
-	
-	def getFullSize(self, match):
-		return "http://images.bcphotoshare.com/storages/%s/large.jpg" % match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://images.bcphotoshare.com/storages/%s/large.jpg" % match.group(1)
-	
-	def getThumbnail(self, match):
-		return "http://images.bcphotoshare.com/storages/%s/thumb180.jpg" % match.group(1)
+class PhotoShare(Resolver):
+    @property
+    def service_name(self):
+        return "Big Canvas PhotoShare"
+    
+    @property
+    def regex_str(self):
+        return r"^http://(?:www\.)?bcphotoshare\.com/photos/\d+/(\d+)/?(?:\?.*)?$"
+    
+    #large.jpgとoriginal.jpgは同じ
+    #拡張子は関係ない
+
+    #重い。画像取得できないし、事実上閉鎖してる感じある。
+    
+    def get_full(self, match):
+        return "http://images.bcphotoshare.com/storages/%s/large.jpg" % match.group(1)
+
+    def get_full_https(self, match):
+        return None
+    
+    def get_large(self, match):
+        return "http://images.bcphotoshare.com/storages/%s/large.jpg" % match.group(1)
+
+    def get_large_https(self, match):
+        return None
+    
+    def get_thumb(self, match):
+        return "http://images.bcphotoshare.com/storages/%s/thumb180.jpg" % match.group(1)
+
+    def get_thumb_https(self, match):
+        return None
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
