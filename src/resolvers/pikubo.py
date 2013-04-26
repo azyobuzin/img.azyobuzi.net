3b0b7aed269abb767e324cb34db0736a633f60e2
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-04-26 18:04:52 +0900

    Pikubo を実装

--- src/resolvers/pikubo.py
+++ src/resolvers/pikubo.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
 
-import re
+from resolvers import *
 
-class pikubo:
-	def __str__(self):
-		return "Pikubo"
-	
-	regexStr = "^https?://(?:www\\.)?pikubo\\.jp/photo/(\\w+)(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	def getFullSize(self, match):
-		return "http://pikubo.jp/p/l/" + match.group(1)
-	
-	def getLargeSize(self, match):
-		return "http://pikubo.jp/p/l/" + match.group(1)
-	
-	def getThumbnail(self, match):
-		return "http://pikubo.jp/p/q/" + match.group(1)
+class Pikubo(Resolver):
+    @property
+    def service_name(self):
+        return "Pikubo"
+    
+    @property
+    def regex_str(self):
+        return r"^https?://(?:www\.)?pikubo\.jp/photo/([\w\-]+)(?:\?.*)?$"
+    
+    def get_full(self, match):
+        return "http://pikubo.jp/p/l/" + match.group(1)
+
+    def get_full_https(self, match):
+        return "https://pikubo.jp/p/l/" + match.group(1)
+    
+    def get_large(self, match):
+        return self.get_full(match)
+
+    def get_large_https(self, match):
+        return self.get_full_https(match)
+    
+    def get_thumb(self, match):
+        return "http://pikubo.jp/p/q/" + match.group(1)
+
+    def get_thumb_https(self, match):
+        return "https://pikubo.jp/p/q/" + match.group(1)
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
