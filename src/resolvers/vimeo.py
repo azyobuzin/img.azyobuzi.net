ef2a7c6a0c419567be38880e9220d26f66879be2
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-05-15 23:36:25 +0900

    Vimeo を実装

--- src/resolvers/vimeo.py
+++ src/resolvers/vimeo.py
@@ -1,65 +1,65 @@
 # -*- coding: utf-8 -*-
 
-import re
 import json
 import urllib2
-import MySQLdb
-from private_constant import *
 
-class vimeo:
-	def __str__(self):
-		return "Vimeo"
-	
-	regexStr = "^https?://(?:www\\.)?vimeo\\.com/(\\d+)/?(?:\\?.*)?$"
-	regex = re.compile(regexStr, re.IGNORECASE)
-	
-	last = None
-	
-	def getUriData(self, match):
-		id = match.group(1)
-		
-		if self.last is not None and str(self.last[0]) == id:
-			return self.last
-		
-		db = MySQLdb.connect(user=dbName, passwd=dbPassword, db=dbName, charset="utf8")
-		c = db.cursor()
-		
-		c.execute("SELECT * FROM vimeo WHERE id = " + db.literal(id))
-		sqlResult = c.fetchone()
-		
-		if sqlResult is None:
-			httpRes = None
-			
-			try:
-				httpRes = urllib2.urlopen("http://vimeo.com/api/v2/video/%s.json" % id)
-			except:
-				return None
-			
-			j = json.loads(httpRes.read())[0]
-			
-			small = j["thumbnail_small"]
-			large = j["thumbnail_large"]
-			
-			c.execute("INSERT INTO vimeo VALUES (%s, %s, %s)"
-				% (db.literal(id), db.literal(small), db.literal(large))
-			)
-			
-			db.commit()
-			
-			self.last = [id, small, large]
-		else:
-			self.last = sqlResult
-		
-		return self.last
-	
-	def getFullSize(self, match):
-		data = self.getUriData(match)
-		return data[2] if data is not None else None
-	
-	def getLargeSize(self, match):
-		data = self.getUriData(match)
-		return data[2] if data is not None else None
-	
-	def getThumbnail(self, match):
-		data = self.getUriData(match)
-		return data[1] if data is not None else None
+from resolvers import *
+
+class Vimeo(StoringResolver):
+    @property
+    def service_name(self):
+        return "Vimeo"
+    
+    @property
+    def regex_str(self):
+        return r"^https?://(?:www\.)?vimeo\.com/(\d+)/?(?:\?.*)?$"
+
+    def get_parameters(self, match):
+        return match.group(1)
+    
+    def _work(self, param, cursor):
+        table = "vimeo"
+        columns = ["id", "small", "large"]
+        result = self.select_one(cursor, table, columns[1:], {columns[0]: param})
+        if result:
+            return dict(zip(columns[1:], result))
+        
+        try:
+            response = urllib2.urlopen("http://vimeo.com/api/v2/video/%s.json" % param)
+        except urllib2.HTTPError as e:
+            if e.code == 404:
+                raise PictureNotFoundError()
+            else:
+                raise e
+            
+        j = json.load(response)[0]
+            
+        small = j["thumbnail_small"]
+        large = j["thumbnail_large"]
+            
+        self.insert_all(cursor, table, (param, small, large))
+        return dict(zip(columns[1:], (small, large)))
+    
+    def get_full(self, match):
+        return self.work(match)["large"]
+
+    def get_full_https(self, match):
+        return self.get_full(match).replace("http://", "https://", 1)
+    
+    def get_large(self, match):
+        return self.get_full(match)
+
+    def get_large_https(self, match):
+        return self.get_full_https(match)
+
+    def get_thumb(self, match):
+        return self.work(match)["small"]
+
+    def get_thumb_https(self, match):
+        return self.get_thumb(match).replace("http://", "https://", 1)
+
+    def get_video(self, match):
+        return None
+
+    def get_video_https(self, match):
+        return None
