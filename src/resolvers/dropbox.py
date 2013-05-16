787fc8fe03cb57558e067d26c29f573a053d5fdf
azyobuzin <azyobuzin@users.sourceforge.jp>
2013-05-16 17:41:33 +0900

    Dropbox の _work まわりを最適化

--- src/resolvers/dropbox.py
+++ src/resolvers/dropbox.py
@@ -14,34 +14,33 @@ class Dropbox(StoringResolver):
         return r"^https?://(?:(?:www\.|dl\.)?dropbox\.com/s/(\w+)/([\w\-\.%]+\.(?:jpeg?|jpg|png|gif|bmp|dib|tiff?))|(?:www\.)?db\.tt/(\w+))/?(?:\?.*)?$"
 
     def get_parameters(self, match):
-        return {"token": match.group(1), "filename": match.group(2), "shorten": match.group(3)}
+        return match.group(3)
 
     def _work(self, param, cursor):
-        if param["shorten"]:
-            table = "dropbox"
-            columns = ["shorten", "expanded"]
-            result = self.select_one(cursor, table, columns[1:], {columns[0]: param["shorten"]})
-            if result:
-                param = self.get_parameters(self.regex.match(result[0]))
-            else:
-                try:
-                    response = urllib2.build_opener(DontRedirectHandler).open(
-                        Request2("http://db.tt/" + param["shorten"], method="HEAD")
-                    )
-                except urllib2.HTTPError as e:
-                    if e.code == 404:
-                        raise PictureNotFoundError()
-                    else:
-                        raise e
+        table = "dropbox"
+        columns = ["shorten", "expanded"]
+        result = self.select_one(cursor, table, columns[1:], {columns[0]: param})
+        if result:
+            match = self.regex.match(result[0])
+        else:
+            try:
+                response = urllib2.build_opener(DontRedirectHandler).open(
+                    Request2("http://db.tt/" + param, method="HEAD")
+                )
+            except urllib2.HTTPError as e:
+                if e.code == 404:
+                    raise PictureNotFoundError()
+                else:
+                    raise e
 
-                expanded = response.headers["location"]
-                self.insert_all(cursor, table, (param["shorten"], expanded))
-                param = self.get_parameters(self.regex.match(expanded))
+            expanded = response.headers["location"]
+            self.insert_all(cursor, table, (param, expanded))
+            match = self.regex.match(expanded)
 
-        return "https://dl.dropbox.com/s/%(token)s/%(filename)s" % param
+        return "https://dl.dropbox.com/s/%s/%s" % (match.group(1), match.group(2))
 
     def get_full(self, match):
-        return self.work(match)
+        return self.work(match) if match.group(3) else "https://dl.dropbox.com/s/%s/%s" % (match.group(1), match.group(2))
 
     def get_full_https(self, match):
         return self.get_full(match).replace("https://", "http://", 1)
