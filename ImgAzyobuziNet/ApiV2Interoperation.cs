c8e725a66291db2dccf87636bc54aebcb5323717
azyobuzin <azyobuzin@users.sourceforge.jp>
2017-05-10 20:29:24 +0900

    whitelist

--- ImgAzyobuziNet/ApiV2Interoperation.cs
+++ ImgAzyobuziNet/ApiV2Interoperation.cs
@@ -11,6 +11,44 @@ namespace ImgAzyobuziNet
         private readonly InteroperationOptions _options;
         private readonly IHttpClient _httpClient;
 
+        private readonly string[] s_serviceNameWhitelist =
+        {
+            "携帯百景",
+            "飯テロ.in",
+            "My365",
+            "MyPix",
+            "ニコニコ動画",
+            "ニコニコ静画",
+            "OneDrive",
+            "Ow.ly",
+            "Path",
+            "Pckles",
+            "PHOTOHITO",
+            "Photomemo",
+            "Big Canvas PhotoShare",
+            "フォト蔵",
+            "PIAPRO",
+            "Pikubo",
+            "pixiv",
+            "Shamoji",
+            "SkyDrive",
+            "Streamzoo",
+            "TINAMI",
+            "Tumblr",
+            "つなビィ",
+            "ついっぷるフォト",
+            "TwitCasting",
+            "Twitgoo",
+            "TwitrPix",
+            "Twitter",
+            "Ustream.tv",
+            "Via.Me",
+            "Vimeo",
+            "Vine",
+            "yfrog",
+            "YouTube"
+        };
+
         public ApiV2Interoperation(InteroperationOptions options, IHttpClient httpClient)
         {
             this._options = options;
