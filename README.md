ab8c2df31c108abb80c68361a5ba4b8dd02b361e
azyobuzin <azyobuzin@users.sourceforge.jp>
2019-01-04 23:54:51 +0900

    DBの接続設定を環境変数から取得するようにした

--- README.md
+++ README.md
@@ -6,17 +6,15 @@
 ## ブランチについて ##
 master は本番サーバーで稼働中のもの、 dev は機能追加・バグ修正のテスト中または公開待機中のものです。
 
-## ミラーサイトの作り方 ##
-- Apache ・ mod_wsgi ・ Python 2.7（2.6 でも動くかも）・ MySQL 5 が実行できるサーバーを用意する
+## 動かしかた ##
+- Python 2.7（2.6 でも動くかも）・ MySQL 5 が実行できるサーバーを用意する
 - ライブラリをインストールする
     - [python-oauth2](http://pypi.python.org/pypi/oauth2)
     - [MySQL-python](http://pypi.python.org/pypi/MySQL-python)
     - [Werkzeug](http://pypi.python.org/pypi/Werkzeug)
 - src の中身を放り込む
-- mirroring/private_constant.py を resolvers ディレクトリに突っ込み、環境に合わせて db_host, db_port, db_name, db_user, db_password を書き換える
+- 環境変数 IMGAZYOBUZI_DB_HOST, IMGAZYOBUZI_DB_POR, IMGAZYOBUZI_DB_NAME ,IMGAZYOBUZI_DB_USER, IMGAZYOBUZI_DB_PASSWORD をいい感じに設定する
 - dbsnapshot_struct.sql を MySQL で実行
-- mod_wsgi を設定（WSGIScriptAlias だけで大丈夫です）
+- gunicorn で動かす
 
 これで多分動くと思います。
-
-本サーバーが負担高めなので、多くのトラフィックが発生すると予想される場合は、ミラーサイトにご協力お願いします。
--- src/resolvers/private_constant.py
+++ src/resolvers/private_constant.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
+import os
 import oauth2
 
-db_host = "<Your Database Host>"
-db_port = <Your Database Port>
-db_name = "<Your Database Name>"
-db_user = "<Your Database User Name>"
-db_password = "<Your Database Password>"
+db_host = os.getenv("IMGAZYOBUZI_DB_HOST", "localhost")
+db_port = int(os.getenv("IMGAZYOBUZI_DB_PORT", "3306"))
+db_name = os.getenv("IMGAZYOBUZI_DB_NAME")
+db_user = os.getenv("IMGAZYOBUZI_DB_USER")
+db_password = os.getenv("IMGAZYOBUZI_DB_PASSWORD")
 
 consumer_key_500px = "jDMkZjOXcidZZex6lhloa95YRnZDVUQhrxX0IHKv"
 flickr_api_key = "341194ea58132794a8d6730b19f92757"
@@ -19,3 +20,10 @@ twitter_client = oauth2.Client(
     oauth2.Consumer(key="uiYQy5R2RJFZRZ4zvSk7A", secret="qzDldacVrcyXbp8pBerf1LBfnQXmkPKmyLVGGLus8"),
     oauth2.Token(key="862962650-rIcjsj0j9ZJ8khPVA8jZTtEJuq7YYDBDpx6fOAgb", secret="kbMQjdVldI6tFOST3SVjmyAtG1D0oCkCpL6vBv1FtA")
 )
+
+__all__ = [
+    "db_host", "db_port", "db_name", "db_user", "db_password",
+    "consumer_key_500px", "flickr_api_key", "tinami_api_key",
+    "tumblr_api_key", "ustream_api_key",
+    "twitter_client"
+]
